9e6d4e9dceac70d48227c908cdfcf68a6f34d061
dipu0 <c.dipu0@gmail.com>
2024-02-13 23:53:12 +0600

    [WIP] Logout three dot with settings

--- lib/features/home_page/presentation/home_page.dart
+++ lib/features/home_page/presentation/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_setup/features/auth/presentation/login_page.dart';
+import 'package:flutter_firebase_setup/features/auth/presentation/signup_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -28,7 +31,32 @@ class _HomePageState extends State<HomePage>
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
+         actions: <Widget>[
+      PopupMenuButton<int>(
+          onSelected: (item) => handleClick(item),
+          itemBuilder: (context) => [
+            PopupMenuItem<int>(value: 0, child: Text('Logout')),
+            PopupMenuItem<int>(value: 1, child: Text('Settings')),
+          ],
+      ),
+         ],
       ),
     );
   }
-}
+
+  _logOut() async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => const LogInPage()));
+  }
+
+  void handleClick(int item) {
+  switch (item) {
+    case 0:
+      _logOut();
+      break;
+    case 1:
+      break;
+  }
+  }
+    }
