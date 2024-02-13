36fe983df34eb12eb44cc429b849ae278ebd4ff1
dipu0 <c.dipu0@gmail.com>
2024-02-13 23:32:21 +0600

    reset passward

--- lib/features/auth/presentation/forgot_password.dart
+++ lib/features/auth/presentation/forgot_password.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_setup/features/auth/widget/ui_helper.dart';
 
@@ -21,9 +22,26 @@ class _ForgotPasswordState extends State<ForgotPassword> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           UiHelper.customTextField(emailController, 'Email', Icons.email),
-          UiHelper.customButton("Reset", () {})
+          UiHelper.customButton("Reset", () {
+            _resetPassword(emailController.text);
+          }),
         ],
       )),
     );
+  }
+
+  _resetPassword(String email) async {
+    if (email.isEmpty) {
+      UiHelper.customAlartDialog(context, 'Error', 'Please fill all fields');
+      return;
+    }
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+      UiHelper.customAlartDialog(context, 'Success', 'Reset Email Sent');
+      await Future.delayed(const Duration(seconds: 2));
+      Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
+      UiHelper.customAlartDialog(context, 'Error', e.message.toString());
+    }
   }
 }
